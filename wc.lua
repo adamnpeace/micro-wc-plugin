c92565b646a908e61d0d400b3f5d04475f79a292
Adam Peace <adam.n.peace@gmail.com>
2016-10-30 16:13:29 +0000

    Initial Code

---
+++ wc.lua
@@ -0,0 +1,13 @@
+VERSION = "1.0.0"
+
+MakeCommand("wc", "wc.wordCount", 0)
+
+function wordCount ()
+	buffer = CurView().Buf	--Pulls working screen buffer from editor view
+	charCount = buffer:len() --Finds length of buffer
+	bufstr = tostring(buffer)
+	_, buflen = bufstr:gsub("[^%s]+","") --Lua patterns:"[^%s]"" relates to NOT of space chars, "+" removes repeated chars (see: lua.org/pil/20.2.html)
+	messenger:Message("Words:"..buflen.."  Characters:"..charCount)
+end
+
+
