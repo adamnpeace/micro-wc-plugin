09f9151dbab250ce648a0fa015e00d1b552ebfc7
bananaapple <15378-bananaapple@users.noreply.gitgud.io>
2020-05-15 14:39:41 +0200

    Add version 1.2.0

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+Version 1.2.0
++ Now counts lines
++ Added Support for counting lines, words and characters in selection
---
+++ wc.lua
@@ -0,0 +1,36 @@
+VERSION = "1.2.0"
+
+local micro = import("micro")
+local config = import("micro/config")
+local util = import("micro/util")
+
+function init()
+    config.MakeCommand("wc", wordCount, config.NoComplete)
+    config.AddRuntimeFile("wc", config.RTHelp, "help/wc.md")
+    config.TryBindKey("F5", "lua:wc.wordCount", false)
+end
+
+function wordCount(bp)
+	-- Buffer of selection/whole document
+	local buffer
+	--Get active cursor (to get selection)
+	local cursor = bp.Buf:GetActiveCursor()
+	--If cursor exists and there is selection, convert selection byte[] to string
+	if cursor and cursor:HasSelection() then
+		buffer = util.String(cursor:GetSelection())
+	else
+	--no selection, convert whole buffer byte[] to string
+    	buffer = util.String(bp.Buf:Bytes())
+    end
+    --length of the buffer/selection (string) (non utf-8 friendly right now)
+	charCount = buffer:len()
+	--Get word/line count using gsub's number of substitutions
+	-- number of substitutions, pattern: %S+ (more than one non-whitespace characters)
+	local _ , wordCount = buffer:gsub("%S+","")
+	-- number of substitutions, pattern: \n (number of newline characters)
+	local _, lineCount = buffer:gsub("\n", "")
+	--add one to line count (since we're counting separators not lines above)
+	lineCount = lineCount + 1
+	--display the message
+	micro.InfoBar():Message("Lines:" .. lineCount .. "  Words:"..wordCount.."  Characters:"..charCount)
+end
